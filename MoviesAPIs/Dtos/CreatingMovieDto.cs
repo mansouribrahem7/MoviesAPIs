e3522b6df316a23daeaead212aefd99b99c10444
Your Name <you@example.com>
2023-07-31 18:58:48 +0300

    max length to 2000

--- MoviesAPIs/Dtos/CreatingMovieDto.cs
+++ MoviesAPIs/Dtos/CreatingMovieDto.cs
@@ -6,7 +6,7 @@ namespace MoviesAPIs.Dtos
 	{
 		public string Title { get; set; } = string.Empty;
 		public int Year { get; set; }
-		[MaxLength(3000)]
+		[MaxLength(2000)]
 		public string StoryLine { get; set; }
 		public double Rate { get; set; }
 		public IFormFile? Poster { get; set; }
